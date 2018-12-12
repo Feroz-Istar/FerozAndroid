7e8a46767833eb4281d7f8c7817145776c71739f
feroz-istar <feroz@istarindia.com>
2018-12-12 16:09:01 +0530

    toobar with searchview

---
+++ app/src/main/java/sample/androido/com/myapplication/landing/toolbar/ToolBarActivity.java
@@ -0,0 +1,91 @@
+package sample.androido.com.myapplication.landing.toolbar;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import sample.androido.com.myapplication.R;
+
+public class ToolBarActivity extends AppCompatActivity {
+    private String mSearchString;
+    SearchView searchView;
+    @BindView(R.id.toolbar2)
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_bar);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.searchable, menu);
+        final MenuItem item = menu.findItem(R.id.action_search);
+        searchView = (SearchView) MenuItemCompat.getActionView(item);
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        EditText editText = (EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        editText.setTextColor(Color.WHITE);
+        editText.setHintTextColor(Color.WHITE);
+        ImageView searchButton = (ImageView) searchView.findViewById(android.support.v7.appcompat.R.id.search_button);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            searchButton.setImageTintList(ColorStateList.valueOf(getResources().getColor(R.color.white)));
+        }
+        if (mSearchString != null && !mSearchString.isEmpty()) {
+            new Handler().post(new Runnable() {
+                @Override
+                public void run() {
+                    searchView.clearFocus();
+                    searchView.setIconified(false);
+                    item.expandActionView();//or MenuItemCompat.expandActionView for older devices
+                    searchView.setQuery(mSearchString, true);
+                    searchView.requestFocus();
+
+
+                }
+            });
+        }
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                return true;
+            }
+        });
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        item.setOnActionExpandListener( new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return true;
+            }
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                return true; // Return true to collapse action view
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
